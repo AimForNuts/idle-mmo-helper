778ec3294a2d6231195e52eaa0d25c5774e54483
AimForNuts <josepsloliveira@gmail.com>
2024-09-10 23:06:51 +0100

    Added to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component'
+import { HeaderComponent } from './header/header.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent
-  ],
-  templateUrl: './app.component.html'
+  imports: [RouterOutlet, HeaderComponent, CalculatorComponent],
+  templateUrl: './app.component.html',
 })
-export class AppComponent { }
+export class AppComponent {}
