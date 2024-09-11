44eb13504225f09788ae59fc739a2c7b07d3db9a
AimForNuts <josepsloliveira@gmail.com>
2024-09-11 11:36:53 +0100

    Alterada estrutura e adicionado routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { BattleComponent } from './battle/battle.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'main-page', component: MainPageComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'battle', component: BattleComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MainPageComponent } from '../main-page/main-page.component';
 import { CalculatorComponent } from '../calculator/calculator.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-component',
   standalone: true,
-  imports: [MainPageComponent, CalculatorComponent],
+  imports: [RouterModule, MainPageComponent, CalculatorComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
